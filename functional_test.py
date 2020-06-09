63eb1dd4d308b830ab44f0126f55a3757dc758ce
bazzaiskl <48783518+bazzaiskl@users.noreply.github.com>
2020-06-09 11:42:43 +0930

    functional test with class

--- functional_test.py
+++ functional_test.py
@@ -1,17 +1,27 @@
 from selenium import webdriver
+import unittest
 
-#stacey decides she wants to use firefox
-browser = webdriver.Firefox()
-#then she decides to go to my blog cos she wants to be cool like me (chad)
-browser.get('http://127.0.0.1:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-#she notices that the title says blog, which is what she wants
-assert 'blog' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_view_blog_post_in_detail(self):
+        #stacey decides to go to my blog cos she wants to be cool like me (chad)
+        self.browser.get('http://127.0.0.1:8000')
 
-#she looks to see if there are submited blog posts
+        #she notices that the title says blog, which is what she wants
+        self.assertIn('blog',self.browser.title)
+        self.fail('Finish Test!')
 
-#then she decides to click on one to see it bigger
+        #she looks to see if there are submited blog posts
 
-#she likes the post cos it made her cool
+        #then she decides to click on one to see it bigger
 
-browser.quit()
+        #she likes the post cos it made her cool
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
