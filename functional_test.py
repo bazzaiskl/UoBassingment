76ee31953f01b096d15f7b2d7b040c7f21d164d1
bazzaiskl <48783518+bazzaiskl@users.noreply.github.com>
2020-06-09 11:30:49 +0930

    basic functional test

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
 
+#stacey decides she wants to use firefox
 browser = webdriver.Firefox()
+#then she decides to go to my blog cos she wants to be cool like me (chad)
 browser.get('http://127.0.0.1:8000')
 
-assert 'Django' in browser.title
+#she notices that the title says blog, which is what she wants
+assert 'blog' in browser.title
+
+#she looks to see if there are submited blog posts
+
+#then she decides to click on one to see it bigger
+
+#she likes the post cos it made her cool
+
+browser.quit()
