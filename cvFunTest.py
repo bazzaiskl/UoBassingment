3834c5c9b0dcae90bd1c490db715d060e7110ca3
bazzaiskl <48783518+bazzaiskl@users.noreply.github.com>
2020-06-22 12:04:55 +0930

    working pre error

--- cvFunTest.py
+++ cvFunTest.py
@@ -3,12 +3,14 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
+
 class creationTest(unittest.TestCase):
+	
     def setUp(self):
-        self.browser = webdriver.Firefox()
+		self.browser = webdriver.Firefox()
 
     def tearDown(self):
-        #this try/excpets checks for the error page and doesnt close it if its there
+        # this try/excpets checks for the error page and doesnt close it if its there
         try:
             errorPage = self.browser.find_element_by_id('summary')
             print("no close")
@@ -17,28 +19,19 @@ class creationTest(unittest.TestCase):
             pass
 
 
-	def test_edit_post(self):
-		pass
-
-
     def test_can_add_info_not_logged_in(self):
-        #stacey wants to make an online cv cos she needs a job (broke af)
-        #she goes to a website to start creating
-        self.browser.get('http://127.0.0.1:8000/cv')
+        # stacey wants to make an online cv cos she needs a job (broke af)
+        # she goes to a website to start creating
+    	self.browser.get('http://127.0.0.1:8000/cv')
 
-        #she sees and clicks on a new cv button
+        # she sees and clicks on a new cv button
         new_cv = self.browser.find_element_by_class_name('new_cv_button').text
         self.assertIn('new', new_cv)
         new_cv_button = self.browser.find_element_by_class_name('new_cv_button')
         new_cv_button.click()
         time.sleep(2)
-#-- new page --#
-       
 
-
-
-
-        #she logs in so she knows its her cv shes making
+        # she logs in so she knows its her cv shes making
         self.assertIn('Login',self.browser.title)
         username = self.browser.find_element_by_name('username')
         password = self.browser.find_element_by_name('password')
@@ -46,11 +39,10 @@ class creationTest(unittest.TestCase):
         username.send_keys('horatioPistachio')
         password.send_keys('boysarebackintown') #this is just for test, better luck next time
         password.send_keys(Keys.ENTER)
-        time.sleep(2)
-#-- new page --#        
+        time.sleep(2)       
 
-        #she sees a box and enters her personal details,address, name ,phone number
-        #email should already be there from login
+        # she sees a box and enters her personal details,address, name ,phone number
+        # email should already be there from login
         self.assertIn('New CV', self.browser.find_element_by_tag_name('h2').text)
 
         name = self.browser.find_element_by_name('name')
@@ -59,18 +51,18 @@ class creationTest(unittest.TestCase):
         email = self.browser.find_element_by_name('email')
 
 
-        #she sees an objectives box and enters her goal
+        # she sees an objectives box and enters her goal
         objective = self.browser.find_element_by_name('objective')
 
-        #she adds a list of skills and personal qualities
+        # she adds a list of skills and personal qualities
         skills = self.browser.find_element_by_name('skills') #-- update to a list style, rather that text field-- maybe hit enter between things
 
 
-        #she adds a list of interests
+        # she adds a list of interests
         interests = self.browser.find_element_by_name('interests')
 
 
-        #she fills out the form
+        # she fills out the form
         name.send_keys("Stacey Shortcakes")
         address.send_keys("23 bakers avenue, cakelton")
         phone_number.send_keys("0385934811")
@@ -80,13 +72,12 @@ class creationTest(unittest.TestCase):
         interests.send_keys("baking\neggs\nfootball\nshoe making")
 
        
-        #she clicks on submit personal details
+        # she clicks on submit personal details
         
-#-- new page --#
         # she sees shes at the experience part
         
 
-        #it asks and she gives a job title, company, years active and a decription of responabilites
+        # it asks and she gives a job title, company, years active and a decription of responabilites
         job_title = self.browser.find_element_by_name('job_title')
         company = self.browser.find_element_by_name('company')
         start_year = self.browser.find_element_by_name('start_year')
@@ -101,16 +92,15 @@ class creationTest(unittest.TestCase):
 
 
 
-        #she has the option to add another experience
+        # she has the option to add another experience
 
-        #she clicks add another
+        # she clicks add another
 
-        #its askes for stuff and she fills it out
+        # its askes for stuff and she fills it out
         
-#-- new page --#
-        #she creates a new education 
+        # she creates a new education 
        
-        #it asks and she adds name of program, inisition of programme, adds years active and a description
+        # it asks and she adds name of program, inisition of programme, adds years active and a description
         program = self.browser.find_element_by_name('program')
         insitution = self.browser.find_element_by_name('insitution')
         start_year_ed = self.browser.find_element_by_name('start_year_ed')
@@ -123,38 +113,31 @@ class creationTest(unittest.TestCase):
         end_year_ed.send_keys('1/1/2020')
         education_description.send_keys('they let us bake a cake AND eat it too')
 
-        #she sees a box to add a list of her school and university involvement
+        # she sees a box to add a list of her school and university involvement
         involvement  = self.browser.find_element_by_name('involvement')
         involvement.send_keys('granny house comp 2020\neating comp\nwake and bake club')
 
-        #she sees a box to add a list of awards
+        # she sees a box to add a list of awards
         awards = self.browser.find_element_by_name('awards')
         awards.send_keys('grannys house comp 2020 3rd place\nbest mud cake\ncoolest flambe')
         
-        
-
-#-- new page --#
-        #she creates a new reference
+        # she creates a new reference
         ref_name = self.browser.find_element_by_name('ref_name')
         ref_position = self.browser.find_element_by_name('ref_position')
         ref_company = self.browser.find_element_by_name('ref_company')
         ref_contact = self.browser.find_element_by_name('ref_contact')
-        #she adds name, title, their position, their compamny and a phone number/ email/ other contact
+        # she adds name, title, their position, their compamny and a phone number/ email/ other contact
         ref_name.send_keys('granny')
         ref_position.send_keys('chief')
         ref_company.send_keys('granny wake n bake')
         ref_contact.send_keys('oldNan@wake2Bake.com')
-        #she submits the cv
+        # she submits the cv
 
         submitPersonalDetail = self.browser.find_element_by_tag_name('button')
         submitPersonalDetail.click()
         time.sleep(3)
-        #she is able to see a nicely formated cv in cv homepage
+        # she is able to see a nicely formated cv in cv homepage
         self.assertIn('Stacey Shortcakes', self.browser.find_element_by_tag_name('h2').text)
-
         
-
-    
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    if __name__ == '__main__':
+        unittest.main(warnings='ignore')
